62896d7b24902d9194cdfdac339dee622d0078c0
Albert Hattingh <albert.h0504@gmail.com>
2019-03-23 16:13:41 +0200

    Window position changed to appear in the center of the screen when app starts.

--- EncryptExpress/Form1.Designer.cs
+++ EncryptExpress/Form1.Designer.cs
@@ -171,6 +171,7 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EncryptExpress";
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
